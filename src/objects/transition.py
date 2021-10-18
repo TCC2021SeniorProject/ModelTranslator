6340b5440a223d0424ad37d418e939138783ff5e
MarcoBackman <roni2006@hanmail.net>
2021-10-18 14:09:40 -0400

    Added functionalities

--- src/objects/transition.py
+++ src/objects/transition.py
@@ -1,3 +1,4 @@
+from variable import Variable
 
 """
     Transition's node links cannot be void
@@ -55,3 +56,53 @@ class Transition():
 
     def get_to_id(self):
         return self.transition_to.get_id()
+
+    
+    def perform_comparision(self, notation_index, variable : Variable, given_value):
+        #variable must exist in the model
+        variable_value = variable.get_variable_value()
+
+        if notation_index == 0: # <
+            return variable_value < given_value
+        elif notation_index == 1: # >
+            return variable_value > given_value
+        elif notation_index == 2: # ==
+            return variable_value == given_value
+        elif notation_index <= 3: # <=
+            return variable_value < given_value
+        elif notation_index >= 4: # >=
+            return variable_value < given_value
+
+    # left fixed to variable name (must be a predefined variable name)
+    # middle fixed to notation
+    # right fixed to number/data
+    # line example: x > 10 / x <= 5.2 / battery == 100
+    def parse_guard(self, line):
+        #Anything other than these
+        notation = {'<' : 0,
+                    '>' : 1,
+                    '=' : 2,
+                    '==' : 2,
+                    '<=' : 3,
+                    '>=' : 4}
+
+        stripped_element = line.strip()
+
+        #format error on given parameter
+        if (not (len(stripped_element) == 3)):
+            print("Wrong guard parameter given")
+            return None
+
+        given_variable = stripped_element[0]
+        given_notation = stripped_element[1]
+        given_value = stripped_element[2]
+
+        #check if given variable is declared
+        variable = self.model.get_variable(given_variable)
+        if variable == False:
+            return None
+
+        #compare value based on the notation
+        if notation.has_key(given_notation):
+            #perform comparison and return Ture/False
+            self.perform_comparision(notation[given_notation], variable, given_value)

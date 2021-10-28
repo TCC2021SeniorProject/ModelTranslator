0b0db534156d636a999aa0fc6c0a7970e012f0e9
MarcoBackman <roni2006@hanmail.net>
2021-10-28 01:28:30 -0400

    Fix minor bug

--- data/output.py
+++ data/output.py
@@ -4,49 +4,24 @@ class TestClass:
 		print('Running constructor')
 		self.status1 = 0
 		self.charge1 = 0
-		self.status2 = 0
-		self.charge2 = 0
-		self.status3 = 0
-		self.charge3 = 0
 
 	async def End(self):
 		exit()
 
-	async def Dock(self):
-		await self.Ready()
-		self.mode = 0
-		await self.End()
-
-
-	async def Explore(self):
-		if self.mode == 4 or self.battery < 10 :
-			self.mode = 4
-			await self.Dock()
-		if self.mode == 3 :
-			await self.Clean()
-
-
 	async def Clean(self):
 		if self.mode == 4 or self.battery < 10 :
 			self.mode = 4
-			await self.Dock()
+			await self.End()
 
 
 	async def Ready(self):
 		if self.mode == 3 :
 			await self.Clean()
-		if self.mode == 2 :
-			await self.Explore()
-
-
-	async def Idle(self):
-		if self.battery > 10 and self.mode == 1 :
-			await self.Ready()
 
 
 	async def Start(self):
-		self.mode = 1
-		await self.Idle()
+		if self.mode == 1 and self.battery > 10 :
+			await self.Ready()
 
 
 
